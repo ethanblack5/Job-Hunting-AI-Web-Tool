5f889537f0235955e87f2f640a8e922fa03bc97a
ethanblack5 <112141263+ethanblack5@users.noreply.github.com>
2026-07-08 12:05:34 -0700

    changes for linting

--- python/main.py
+++ python/main.py
@@ -1,29 +1,28 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+#from pydantic import BaseModel
 import httpx
 
 app = FastAPI()
+
 
 @app.get("/fetch-external-data")
 async def get_remoteok_data():
 
     url = "https://remoteok.com/"
-
     async with httpx.AsyncClient() as client:
         try:
             response = await client.get(url)
-
             response.raise_for_status()
-
             return response.json()
-        
+            
         except httpx.HTTPStatusError as exc:
             raise HTTPException(
-                status_code=exc.response.status_code, 
+                status_code=exc.response.status_code,
                 detail=f"External server error: {exc}"
             )
+            
         except httpx.RequestError as exc:
             raise HTTPException(
-                status_code=503, 
+                status_code=503,
                 detail=f"Could not connect to external server: {exc}"
             )
