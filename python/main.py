eb47fde70787779a16164205e11fbb4415c6c5b9
ethanblack5 <112141263+ethanblack5@users.noreply.github.com>
2026-07-08 19:00:39 +0000

    test commit with some basic fastapi implementation

--- python/main.py
+++ python/main.py
@@ -1,1 +1,29 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import httpx
 
+app = FastAPI()
+
+@app.get("/fetch-external-data")
+async def get_remoteok_data():
+
+    url = "https://remoteok.com/"
+
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(url)
+
+            response.raise_for_status()
+
+            return response.json()
+        
+        except httpx.HTTPStatusError as exc:
+            raise HTTPException(
+                status_code=exc.response.status_code, 
+                detail=f"External server error: {exc}"
+            )
+        except httpx.RequestError as exc:
+            raise HTTPException(
+                status_code=503, 
+                detail=f"Could not connect to external server: {exc}"
+            )
